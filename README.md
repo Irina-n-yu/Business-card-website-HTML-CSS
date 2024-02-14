8db7772d9cf3d33f5c985c6fd5aed2cab5cf86c1
Irina-n-yu <159479227+Irina-n-yu@users.noreply.github.com>
2024-02-14 15:18:50 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# AleksandrN-QA.github.io
---
+++ js.js
@@ -0,0 +1,4 @@
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+	alert ('Тестирование - это как игра в шахматы с компьютером, где вы постоянно ищете ходы, чтобы обнаружить его слабости и заставить его королеву побледнеть.')
+})
